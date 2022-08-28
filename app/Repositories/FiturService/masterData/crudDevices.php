81a2f5c1e9b1bf966d22b419e24156069847afe5
syamsulapp <samsul.marif100@gmail.com>
2022-08-28 17:12:54 +0800

    add logic check key apakah key nya ada atau tidak

--- app/Repositories/FiturService/masterData/crudDevices.php
+++ app/Repositories/FiturService/masterData/crudDevices.php
@@ -8,31 +8,37 @@ class crudDevices
 {
     public function add_devices($param, $modelPairing, $builder, $modelDevices, $user)
     {
+        $costum = [
+            'required' => ':attribute jangan di kosongkan'
+        ];
         $validator = Validator::make($param->all(), [
             'key' => 'required',
-        ]);
+        ], $costum);
         if ($validator->fails()) {
             $result = $builder->responData(['errors' => $validator->errors()], 422, 'failed request');
         } else {
             $user = $user->authentikasi();
-            $pair = $modelPairing::where('key', $param->key)->first();
-            if ($pair->table_users_id == $user->id) {
-                if (!$modelDevices::where('table_pairing_key', $pair->key)->first()) {
-                    $modelDevices::create([
-                        'table_pairing_key' => $pair->key,
-                        'watt' => $pair->watt,
-                        'ampere' => $pair->ampere,
-                        'volt' => $pair->volt,
-                        'table_users_id' => $pair->table_users_id,
-                        'table_status_devices_key_status_perangkat' => 1,
-                        'table_schedule_devices_key_status_table_perangkat' => 2,
-                    ]);
-                    $result = $builder->responData(['message' => 'sukses add devices'], 200, 'Successfully Added Data');
+            if ($pair = $modelPairing::where('key', $param->key)->first()) {
+                if ($pair->table_users_id == $user->id) {
+                    if (!$modelDevices::where('table_pairing_key', $pair->key)->first()) {
+                        $modelDevices::create([
+                            'table_pairing_key' => $pair->key,
+                            'watt' => $pair->watt,
+                            'ampere' => $pair->ampere,
+                            'volt' => $pair->volt,
+                            'table_users_id' => $pair->table_users_id,
+                            'table_status_devices_key_status_perangkat' => 1,
+                            'table_schedule_devices_key_status_table_perangkat' => 2,
+                        ]);
+                        $result = $builder->responData(['message' => 'sukses add devices'], 200, 'Successfully Added Data');
+                    } else {
+                        $result = $builder->responData(['message' => 'perangkat ini sudah di tambahkan'], 422, 'failed pairing');
+                    }
                 } else {
-                    $result = $builder->responData(['message' => 'perangkat ini sudah di tambahkan'], 422, 'failed pairing');
+                    $result = $builder->responData(['message' => 'bukan perangkatmu'], 422, 'failed add devices');
                 }
             } else {
-                $result = $builder->responData(['message' => 'bukan perangkatmu'], 422, 'failed add devices');
+                $result = $builder->responData(['message' => 'key tidak di temukan'], 422, 'failed requuest');
             }
         }
 
