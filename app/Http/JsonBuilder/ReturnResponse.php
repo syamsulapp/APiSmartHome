9362e60cb8277d1d2af7fe9b2cc89ccf69c951e0
syamsulapp <samsul.marif100@gmail.com>
2022-09-05 07:06:28 +0800

    format indentasi code json builder

--- app/Http/JsonBuilder/ReturnResponse.php
+++ app/Http/JsonBuilder/ReturnResponse.php
@@ -2,24 +2,25 @@
 
 namespace App\Http\JsonBuilder;
 
-class ReturnResponse {
-    public static function responData($data, int $statusCode = 200 , String $message = 'success data')
-    {
-    	switch($statusCode) {
-    		case 200 :
-    			$respon = [
-    				'statusCode' => $statusCode,
-    				'message' => $message,
-    				'data' => $data,
-    			];
-    		default :
-    			$respon = [
-    				'statusCode' => $statusCode,
-    				'message' => $message,
-    				'data' => $data,
-    			];
+class ReturnResponse
+{
+	public static function responData($data, int $statusCode = 200, String $message = 'success data')
+	{
+		switch ($statusCode) {
+			case 200:
+				$respon = [
+					'statusCode' => $statusCode,
+					'message' => $message,
+					'data' => $data,
+				];
+			default:
+				$respon = [
+					'statusCode' => $statusCode,
+					'message' => $message,
+					'data' => $data,
+				];
 
-    	return response()->json($respon,$statusCode);
-    	}
-    }
+				return response()->json($respon, $statusCode);
+		}
+	}
 }
