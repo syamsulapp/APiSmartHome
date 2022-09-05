453f42080b403125f1b809ce69a745a3d976315b
syamsulapp <samsul.marif100@gmail.com>
2022-09-05 12:50:04 +0800

    error handleing for validasi in schedule devices

--- app/Repositories/FiturService/Schedule_perangkat.php
+++ app/Repositories/FiturService/Schedule_perangkat.php
@@ -11,24 +11,28 @@ class Schedule_perangkat
 {
     public function schedulePerangkat($param, $user, $builder, $modelDevices)
     {
-        $costum = [
-            'required' => ':attribute jangan di kosongkan'
-        ];
-        $validasi = Validator::make($param->all(), [
-            'key' => 'required'
-        ], $costum);
         try {
-            if ($validasi->fails()) {
-                $result = $builder->responData(['errors' => $validasi->errors()], 422, 'failed request');
-            } else {
-                $user = $user->authentikasi();
-                $devicesSchedule = $modelDevices::where('table_pairing_key', $user->id)->first();
-                $result = $devicesSchedule;
-                // $date = new DateTime();
-                // $result = $builder->responData($date->format('H:i:s'));
+            $costum = [
+                'required' => ':attribute jangan di kosongkan'
+            ];
+            $validasi = Validator::make($param->all(), [
+                'key' => 'required'
+            ], $costum);
+            try {
+                if ($validasi->fails()) {
+                    $result = $builder->responData(['errors' => $validasi->errors()], 422, 'failed request');
+                } else {
+                    $user = $user->authentikasi();
+                    $devicesSchedule = $modelDevices::where('table_pairing_key', $user->id)->first();
+                    $result = $devicesSchedule;
+                    // $date = new DateTime();
+                    // $result = $builder->responData($date->format('H:i:s'));
+                }
+            } catch (Exception $error) {
+                $result = $builder->responData(['message' => 'errors siste'], 500, $error);
             }
         } catch (Exception $error) {
-            $result = $builder->responData(['message' => 'errors siste'], 500, $error);
+            $result = $builder->responData(['message' => 'error validasi'], 500, $error);
         }
 
         return $result;
