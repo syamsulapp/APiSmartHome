acf3e3390170ff0c6b0863bf0d47a98673e0fcf6
syamsulapp <samsul.marif100@gmail.com>
2022-09-05 12:47:42 +0800

    first create logic schedule devices

--- app/Repositories/FiturService/Schedule_perangkat.php
+++ app/Repositories/FiturService/Schedule_perangkat.php
@@ -1,11 +1,36 @@
 <?php
 
+
 namespace App\Repositories\FiturService;
+
+use DateTime;
+use Exception;
+use Illuminate\Support\Facades\Validator;
 
 class Schedule_perangkat
 {
-    public function schedulePerangkat($param, $modelSchedule)
+    public function schedulePerangkat($param, $user, $builder, $modelDevices)
     {
-        #write logic code for schedule devices
+        $costum = [
+            'required' => ':attribute jangan di kosongkan'
+        ];
+        $validasi = Validator::make($param->all(), [
+            'key' => 'required'
+        ], $costum);
+        try {
+            if ($validasi->fails()) {
+                $result = $builder->responData(['errors' => $validasi->errors()], 422, 'failed request');
+            } else {
+                $user = $user->authentikasi();
+                $devicesSchedule = $modelDevices::where('table_pairing_key', $user->id)->first();
+                $result = $devicesSchedule;
+                // $date = new DateTime();
+                // $result = $builder->responData($date->format('H:i:s'));
+            }
+        } catch (Exception $error) {
+            $result = $builder->responData(['message' => 'errors siste'], 500, $error);
+        }
+
+        return $result;
     }
 }
