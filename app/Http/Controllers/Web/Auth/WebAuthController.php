9ca0ba827fc1041a58770c46864f11dc5c53e446
samsul <samsul.marif100@gmail.com>
2022-10-21 20:42:08 +0800

    create web login and register

--- app/Http/Controllers/Web/Auth/WebAuthController.php
+++ app/Http/Controllers/Web/Auth/WebAuthController.php
@@ -1,10 +1,27 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Web\Auth;
 
+use App\Http\Controllers\Controller;
+use App\Repositories\Web\Auth\WebAuthRepository;
 use Illuminate\Http\Request;
 
 class WebAuthController extends Controller
 {
-    //
+    protected $webAuth;
+
+
+    public function __construct(WebAuthRepository $webAuth)
+    {
+        $this->webAuth = $webAuth;
+    }
+
+
+    public function login(Request $login)
+    {
+    }
+
+    public function register(Request $register)
+    {
+    }
 }
---
+++ app/Repositories/Web/Auth/WebAuthRepository.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Repositories\Web\Auth;
+
+use App\Repositories\BaseRepository;
+
+class WebAuthRepository extends BaseRepository
+{
+    public function __construct()
+    {
+    }
+
+    public function login($login)
+    {
+    }
+
+    public function register($register)
+    {
+    }
+}
