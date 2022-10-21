5959bc4f0b497dbe343f485f2b5c77d4a486e41c
samsul <samsul.marif100@gmail.com>
2022-10-21 20:51:11 +0800

    fix routes between user fitur and user web

--- routes/web.php
+++ routes/web.php
@@ -29,54 +29,31 @@ $router->group(['prefix' => 'auth'], function () use ($router) {
     });
 });
 
-$router->group(['prefix' => 'fitur', 'middleware' => 'client'], function () use ($router) {
+$router->group(['prefix' => 'user'], function () use ($router) {
 
-    //devices
-    $router->group(['prefix' => 'devices'], function () use ($router) {
-        $router->post('/', 'DevicesController@listDevices');
-        $router->post('detail', 'DevicesController@detailDevices');
-    });
-
-    //pairing perangkat
-    $router->group(['prefix' => 'pairing'], function () use ($router) {
-        $router->post('/', 'DevicesController@listPairing');
-        $router->post('/devices', 'DevicesController@pairingPerangkat');
-    });
-
-    //fitur user
-    $router->group(['prefix' => 'user'], function () use ($router) {
-        $router->group(['prefix' => 'schedule'], function () use ($router) {
-            $router->post('/', 'DevicesController@schedulePerangkat');
-        });
-
-        // hemat daya fitur users
-        $router->group(['prefix' => 'hematDaya'], function () use ($router) {
-            $router->post('/', 'DevicesController@hematDaya');
-        });
-    });
-
-    // master data
-    $router->group(['prefix' => 'master_data'], function () use ($router) {
-        $router->group(['prefix' => 'otomatisasiPerangkat'], function () use ($router) {
-            /** upcoming */
-            $router->get('/add_otomatisasi', 'DevicesController@add_otomatisasi');
-            $router->get('/update_otomatisasi', 'DevicesController@update_otomatisasi');
-            $router->get('/delete_otomatisasi', 'DevicesController@delete_otomatisasi');
-        });
-        $router->group(['prefix' => 'schedulePerangkat'], function () use ($router) {
-            $router->get('/get_schedule', 'DevicesController@get_schedule');
-            $router->post('/add_schedule', 'DevicesController@add_schedule');
-            $router->put('/update_schedule', 'DevicesController@update_schedule');
-            $router->delete('/delete_schedule', 'DevicesController@delete_schedule');
-        });
+    $router->group(['prefix' => 'fitur', 'middleware' => 'client'], function () use ($router) {
+        //devices
         $router->group(['prefix' => 'devices'], function () use ($router) {
-            $router->post('/add_devices', 'DevicesController@add_devices');
+            $router->post('/', 'DevicesController@listDevices');
+            $router->post('detail', 'DevicesController@detailDevices');
         });
-        $router->group(['prefix' => 'role_users'], function () use ($router) {
-            $router->get('/get_role', 'DevicesController@get_role');
-            $router->post('/add_role', 'DevicesController@add_role');
-            $router->put('/update_role', 'DevicesController@update_role');
-            $router->delete('/delete_role', 'DevicesController@delete_role');
+
+        //pairing perangkat
+        $router->group(['prefix' => 'pairing'], function () use ($router) {
+            $router->post('/', 'DevicesController@listPairing');
+            $router->post('/devices', 'DevicesController@pairingPerangkat');
+        });
+
+        //fitur user
+        $router->group(['prefix' => 'user'], function () use ($router) {
+            $router->group(['prefix' => 'schedule'], function () use ($router) {
+                $router->post('/', 'DevicesController@schedulePerangkat');
+            });
+
+            // hemat daya fitur users
+            $router->group(['prefix' => 'hematDaya'], function () use ($router) {
+                $router->post('/', 'DevicesController@hematDaya');
+            });
         });
     });
 
@@ -84,6 +61,29 @@ $router->group(['prefix' => 'fitur', 'middleware' => 'client'], function () use 
         $router->group(['prefix' => 'auth', 'namespace' => 'Auth'], function () use ($router) {
             $router->post('/login', 'WebAuthController@login');
             $router->post('/register', 'WebAuthController@register');
+        });
+        $router->group(['prefix' => 'master'], function () use ($router) {
+            $router->group(['prefix' => 'otomatisasiPerangkat'], function () use ($router) {
+                /** upcoming */
+                $router->get('/add_otomatisasi', 'DevicesController@add_otomatisasi');
+                $router->get('/update_otomatisasi', 'DevicesController@update_otomatisasi');
+                $router->get('/delete_otomatisasi', 'DevicesController@delete_otomatisasi');
+            });
+            $router->group(['prefix' => 'schedulePerangkat'], function () use ($router) {
+                $router->get('/get_schedule', 'DevicesController@get_schedule');
+                $router->post('/add_schedule', 'DevicesController@add_schedule');
+                $router->put('/update_schedule', 'DevicesController@update_schedule');
+                $router->delete('/delete_schedule', 'DevicesController@delete_schedule');
+            });
+            $router->group(['prefix' => 'devices'], function () use ($router) {
+                $router->post('/add_devices', 'DevicesController@add_devices');
+            });
+            $router->group(['prefix' => 'role_users'], function () use ($router) {
+                $router->get('/get_role', 'DevicesController@get_role');
+                $router->post('/add_role', 'DevicesController@add_role');
+                $router->put('/update_role', 'DevicesController@update_role');
+                $router->delete('/delete_role', 'DevicesController@delete_role');
+            });
         });
     });
 });
