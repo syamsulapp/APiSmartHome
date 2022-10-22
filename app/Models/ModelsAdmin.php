cd98746526577ca778d58949fe291ff1cb131a1f
samsul <samsul.marif100@gmail.com>
2022-10-22 08:11:31 +0800

    use softdelete

--- app/Models/ModelsAdmin.php
+++ app/Models/ModelsAdmin.php
@@ -3,9 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ModelsAdmin extends Model
 {
+    use SoftDeletes;
     protected $table = 'admin';
     protected $fillable = ['username', 'password', 'email', 'created_at', 'updated_at', 'role_user_idrole_user'];
 }
