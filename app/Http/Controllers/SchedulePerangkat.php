f75926405f7ef7cd9ef1811edc53f859e10e6b68
samsul <samsul.marif100@gmail.com>
2022-11-02 06:59:03 +0800

    fitur schedule

---
+++ app/Http/Controllers/SchedulePerangkat.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SchedulePerangkat extends Controller
+{
+    public function __construct()
+    {
+    }
+
+    public function index(Request $index)
+    {
+    }
+
+    public function store(Request $store)
+    {
+    }
+
+    public function update(Request $update)
+    {
+    }
+
+    public function delete(Request $delete)
+    {
+    }
+}
---
+++ app/Repositories/FiturService/ScheduleRepository.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Repositories\FiturService;
+
+use App\Repositories\BaseRepository;
+
+class ScheduleRepository extends BaseRepository
+{
+    public function __construct()
+    {
+    }
+
+    public function index($index)
+    {
+    }
+
+    public function store($store)
+    {
+    }
+
+    public function update($update)
+    {
+    }
+
+    public function delete($delete)
+    {
+    }
+}
